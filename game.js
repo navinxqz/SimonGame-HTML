5e7ca9eaf747fd492d37cd276e86032866f997e7
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-10 01:51:17 +0600

    random num

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
 // $(document).on("keydown",function(event){
 //     $("h1").slideToggle();
 // });
+var btncolor = ["red", "blue", "green", "yellow"];
+var randomcolor = btncolor[nextSequence()];
+
+var gamePattern = [];
+gamePattern.push(randomcolor);
+console.log(gamePattern);
+
+function nextSequence(){
+    var rand = Math.floor(Math.random()*4);
+    return rand;
+}
