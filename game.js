5369aad0d9ad2321e67cc58ff1cd732d2fae52f5
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-12 21:29:58 +0600

    playSound() function call

--- game.js
+++ game.js
@@ -6,8 +6,10 @@ nextSequence();
 var userClickedPattern = [];
 $(".btn").on("click", function(){
     var chosenColor = $(this).attr("id");
+
     userClickedPattern.push(chosenColor);
-    console.log(userClickedPattern);
+    playSound(chosenColor);
+    // console.log(userClickedPattern);
 });
 
 function nextSequence(){
@@ -16,7 +18,9 @@ function nextSequence(){
 
     gamePattern.push(randomcolor);
     $("#"+randomcolor).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
-
-    var audio = new Audio("./sounds/"+randomcolor+".mp3");
-    // audio.play();
+    playSound(gamePattern);
+}
+function playSound(name){
+    var audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
 }
